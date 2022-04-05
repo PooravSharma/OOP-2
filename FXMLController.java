b33fb800a3ca8731f3c9b7ad71716e10ea3b7b3e
Poorav Sharma <57639207+PooravSharma@users.noreply.github.com>
2022-04-05 15:46:21 +0800

    Add files via upload
    A4

---
+++ FXMLController.java
@@ -0,0 +1,70 @@
+package wholeworthsgrocery;
+import java.awt.TextField;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+public class FXMLController implements Initializable {
+    @FXML
+    private TextField BreadinputTextbox;
+
+    @FXML
+    private TextField MilkinputTextbox;
+
+    @FXML
+    private TextField egginputTextbox;
+
+    @FXML
+    private TextField overallCost;
+
+    @FXML
+    private TextField totalbreadCost;
+
+    @FXML
+    private TextField totaleggCost;
+
+    @FXML
+    private TextField totalmilkCost;
+
+    @FXML
+    void BreadinputTextbox(ActionEvent event) {
+
+    }
+
+    @FXML
+    void MilkinputTextbox(ActionEvent event) {
+
+    }
+
+    @FXML
+    void egginputTextbox(ActionEvent event) {
+
+    }
+
+    @FXML
+    void getoverallCost(ActionEvent event) {
+
+    }
+
+    @FXML
+    void gettotalbreadCost(ActionEvent event) {
+
+    }
+
+    @FXML
+    void gettotaleggCost(ActionEvent event) {
+
+    }
+
+    @FXML
+    void gettotalmilkCost(ActionEvent event) {
+
+    }
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+}
