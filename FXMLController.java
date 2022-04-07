8ece1934c4c75fb40bf413f037df6e08e692ee4b
Poorav Sharma <57639207+PooravSharma@users.noreply.github.com>
2022-04-07 15:00:21 +0800

    Add files via upload
    Assessment 4

--- FXMLController.java
+++ FXMLController.java
@@ -1,13 +1,16 @@
 package wholeworthsgrocery;
-import java.awt.TextField;
+
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 
 public class FXMLController implements Initializable {
+
     @FXML
     private TextField BreadinputTextbox;
 
@@ -30,39 +33,94 @@ public class FXMLController implements Initializable {
     private TextField totalmilkCost;
 
     @FXML
-    void BreadinputTextbox(ActionEvent event) {
+    private Label myLabel;
 
+   static int egg = 0;
+   static double eggCost = 0.00;
+
+    static int milk = 0;
+   static double milkCost = 0.00;
+
+    static int bread = 0;
+    static double breadCost = 0.00;
+
+    double totalCost;
+
+    @FXML
+    void BreadinputTextbox(KeyEvent event) {
+        bread=0;
+        try {
+            bread = Integer.parseInt(BreadinputTextbox.getText());
+
+            if (bread >= 0) {
+                myLabel.setText("Calculating...");
+            } else {
+                myLabel.setText("Enter a positive whole number");
+            }
+        } catch (NumberFormatException ex) {
+            myLabel.setText("ERROR: Enter number amount!!");
+        } catch (Exception ex) {
+            myLabel.setText("ERROR: Enter correct amount!!");
+        }
+        double single = (bread % 2) * 3.50;
+        double dozen = (bread / 2) * 6.00;
+        breadCost = dozen + single;
+        totalbreadCost.setText("$" + breadCost);
     }
 
     @FXML
-    void MilkinputTextbox(ActionEvent event) {
+    void MilkinputTextbox(KeyEvent event) {
+       milk=0;
+        try {
+            milk = Integer.parseInt(MilkinputTextbox.getText());
 
+            if (milk >= 0) {
+                myLabel.setText("Calculating...");
+            } else {
+                myLabel.setText("Enter a positive whole number");
+            }
+        } catch (NumberFormatException ex) {
+            myLabel.setText("ERROR: Enter number amount!!");
+        } catch (Exception ex) {
+            myLabel.setText("ERROR: Enter correct amount!!");
+        }
+        double single = (milk % 3) * 2.50;
+        double dozen = (milk / 3) * 7.00;
+        milkCost = dozen + single;
+        totalmilkCost.setText("$" + milkCost);
     }
 
     @FXML
-    void egginputTextbox(ActionEvent event) {
+    void egginputTextbox(KeyEvent event) {
+        egg=0;
+        try {
+            egg = Integer.parseInt(egginputTextbox.getText());
 
+            if (egg >= 0) {
+                myLabel.setText("Calculating...");
+            } else {
+                myLabel.setText("Enter a positive whole number");
+            }
+        } catch (NumberFormatException ex) {
+            myLabel.setText("ERROR: Enter number amount!!");
+        } catch (Exception ex) {
+            myLabel.setText("ERROR: Enter correct amount!!");
+        }
+
+        double single = (egg % 12) * 0.50;
+        double dozen = (egg / 12) * 5.00;
+        eggCost = dozen + single;
+        totaleggCost.setText("$" + eggCost);
     }
 
     @FXML
-    void getoverallCost(ActionEvent event) {
-
+    void calculate(ActionEvent event) {
+        totalCost = eggCost + breadCost + milkCost;
+        overallCost.setText("$" + totalCost);
+        myLabel.setText("Done");
     }
+        
 
-    @FXML
-    void gettotalbreadCost(ActionEvent event) {
-
-    }
-
-    @FXML
-    void gettotaleggCost(ActionEvent event) {
-
-    }
-
-    @FXML
-    void gettotalmilkCost(ActionEvent event) {
-
-    }
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
